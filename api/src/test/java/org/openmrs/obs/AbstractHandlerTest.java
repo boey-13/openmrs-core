e06212ec4a83404e38a642ff6a2d0834f0d8b99b
boey-13 <baoyee13@gmail.com>
2025-03-18 22:12:39 +0800

    Update AbstractHandlerTest.java

--- api/src/test/java/org/openmrs/obs/AbstractHandlerTest.java
+++ api/src/test/java/org/openmrs/obs/AbstractHandlerTest.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.obs;
+/**package org.openmrs.obs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -103,4 +103,4 @@ public class AbstractHandlerTest extends BaseContextSensitiveTest {
 		assertEquals(filename, key);
 	}
 	
-}
+}*/
